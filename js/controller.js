3a9130026bc514121d4040cb6bee36384847f0b9
Patrick Chieni <pchieni25@gmail.com>
2018-04-27 20:33:03 +0300

    File Import Changes

--- js/controller.js
+++ js/controller.js
@@ -1,59 +1,54 @@
 angular.module('ToDoApp', ['ToDoApp.controllers']);
 
-                angular.module('ToDoApp.controllers', []).controller('todoController', function($scope) {
-                                $scope.objectIndex = '';
-                                $scope.taskList = [
-                                                  {
-                                                               Name: 'CSS coding',
-                                                               Status: 'DONE',
-                                                               Action: ''
-                                                  },
+angular.module('ToDoApp.controllers', []).controller('todoController', function ($scope) {
+    $scope.objectIndex = '';
+    $scope.taskList = [
+        {
+            Name: 'CSS coding',
+            Status: 'DONE',
+            Action: ''
+        },
+        {
+            Name: 'HTML Coding',
+            Status: 'Pending',
+            Action: ''
+        },
+        {
+            Name: 'Testing',
+            Status: 'Pending',
+            Action: ''
+        }
+    ];
 
-                                                 {
-                                                                Name: 'HTML Coding',
-                                                                Status: 'Pending',
-                                                                Action: ''
-                                                  },
-                                                  {
-                                                                Name: 'Testing',
-                                                                Status: 'Pending',
-                                                                Action: ''
-                                                  }
-                                  ];
+    // search and update task
+    $scope.edit = function (id) {
+        $scope.objectIndex = id;
+        $scope.taskObj = angular.copy($scope.taskList[id]);
+        console.log($scope.objectIndex);
+    }
 
-                                 $scope.edit = function(id) {
-                                 // search and update task
-                                    $scope.objectIndex = id;
-                                                $scope.taskObj = angular.copy($scope.taskList[id]);
-                                                console.log($scope.objectIndex);
-                                }
-                                 
-                                 $scope.delete = function(id) {
-                                     // search record with given id
-										// and delete it
-                                     for(i in $scope.taskList) {
-                                                     if($scope.taskList[i].id == id) {
-                                                                     $scope.taskList.splice(i,1);
-                                                                     $scope.taskList = {};
-                                                     }
-                                     }
+    // search record using given id and delete it
+    $scope.delete = function (id) {
+        for (i in $scope.taskList) {
+            if ($scope.taskList[i].id == id) {
+                $scope.taskList.splice(i, 1);
+                $scope.taskList = {};
+            }
+        }
 
-                     }
+    }
 
-                                $scope.save = function() {
-                                                console.log($scope.objectIndex);
-                                                if($scope.taskList[$scope.objectIndex] == null) {
-                                                              
-                                                                $scope.taskList.push($scope.taskObj);
-                                                } else {
-                                                               
-                                                                $scope.taskList[$scope.objectIndex] = $scope.taskObj;
-                                                }
+    $scope.save = function () {
+        if ($scope.taskList[$scope.objectIndex] == null) {
+            $scope.taskList.push($scope.taskObj);
+        } else {
+            $scope.taskList[$scope.objectIndex] = $scope.taskObj;
+        }
 
-                                                // clear the add record form
-                                                $scope.taskObj = {};
-                                                $scope.objectIndex = '';
-                                }
+        // clear the add record form
+        $scope.taskObj = {};
+        $scope.objectIndex = '';
+    }
 
-                                
-                });
+
+});
